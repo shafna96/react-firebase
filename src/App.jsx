60e480d775c84d01b7616486704ee907bb42116d
shafna96 <sfnmail2@gmail.com>
2023-03-13 17:22:59 +0530

    fb storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
-import { auth, db } from "./config/firebase";
+import { auth, db, storage } from "./config/firebase";
 import {
   getDocs,
   collection,
@@ -10,6 +10,7 @@ import {
   doc,
   updateDoc,
 } from "firebase/firestore";
+import { ref, uploadBytes } from "firebase/storage";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
@@ -21,6 +22,9 @@ function App() {
 
   //Update Title State
   const [updatedTitle, setUpdatedTitle] = useState("");
+
+  //File Upload State
+  const [fileUpload, setFileUpload] = useState(null);
 
   const moviesCollectionRef = collection(db, "movies");
   const getMovieList = async () => {
@@ -65,6 +69,16 @@ function App() {
     await updateDoc(movieDoc, { title: updatedTitle });
   };
 
+  const uploadFile = async () => {
+    if (!fileUpload) return;
+    const filesFolderRef = ref(storage, `projectFiles/${fileUpload.name}`);
+    try {
+      await uploadBytes(filesFolderRef, fileUpload);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="App">
       <Auth />
@@ -101,6 +115,10 @@ function App() {
             <button onClick={() => updateMovie(movie.id)}>Update Title</button>
           </div>
         ))}
+      </div>
+      <div>
+        <input type="file" onChange={(e) => setFileUpload(e.target.files[0])} />
+        <button onClick={uploadFile}>Upload File</button>
       </div>
     </div>
   );
